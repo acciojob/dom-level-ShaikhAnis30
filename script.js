1b214306957a60413c1e1e72baa96d654ed64dee
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-07-27 15:03:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let li = document.getElementById("level");
+let levelText = li.innerText;
+let arr = levelText.split(' ');
+let level = arr[1];
+
+alert(`The level of the element is: ${level}`)
